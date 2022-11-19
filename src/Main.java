1d9f7a7f4cd6b62d34c8b118f90026b3514eafa0
Fabricio Valadares <fabricio.valadares@gmail.com>
2022-11-18 21:08:49 -0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Conta conta; // Declaração da variável do tipo conta
+        conta = new Conta(); // Instância da classe Conta
 
+        conta.setAgencia(123); // Configurando a agência;
+        conta.setNumeroConta(456);
+        conta.depositar(1000);
+        conta.sacar(2000);
+        System.out.println(conta.consultarSaldo());
     }
 }
