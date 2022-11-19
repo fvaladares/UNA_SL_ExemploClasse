08578233e98cd4438720391b427b3e08f6ccaf51
Fabricio Valadares <fabricio.valadares@gmail.com>
2022-11-19 11:10:29 -0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,8 @@ public class Main {
         conta.depositar(1000);
         conta.sacar(2000);
         System.out.println(conta.consultarSaldo());
+
+        Conta conta2 = new Conta(99876, 10002, 100000);
+        System.out.println(conta2.consultarSaldo());
     }
 }
